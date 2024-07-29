559705ec1b9c73cd1e1e5f19c62756c16366884c
valentinaduarte <valentinadc1@hotmail.com>
2024-07-29 11:08:53 -0500

    Adding product entity

---
+++ src/main/java/com/riwi/assentment_project/domain/entities/Product.java
@@ -0,0 +1,36 @@
+package com.riwi.assentment_project.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Float price;
+
+    @Column(name = "stock", nullable = false)
+    private int stock;
+
+    
+}
