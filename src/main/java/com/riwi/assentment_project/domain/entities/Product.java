fa331109808c1bf924a130505c0b5c2f13f3a3d1
valentinaduarte <valentinadc1@hotmail.com>
2024-07-29 11:29:02 -0500

    Adding database seeder in order to initialize 10 products and adding builder decorator to Product entity

--- src/main/java/com/riwi/assentment_project/domain/entities/Product.java
+++ src/main/java/com/riwi/assentment_project/domain/entities/Product.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "product")
 public class Product {
     @Id
