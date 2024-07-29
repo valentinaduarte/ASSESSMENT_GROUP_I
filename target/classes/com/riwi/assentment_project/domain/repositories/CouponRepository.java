2b25f6d71dd9dda23fc8a9b4f03ef186f1bdeb20
Jeronimo <jerocruz76@gmail.com>
2024-07-29 10:21:18 -0500

    repository coupon

---
+++ target/classes/com/riwi/assentment_project/domain/repositories/CouponRepository.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.riwi.assentment_project.domain.entities.Coupon;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+}
