7f8929cb10de00fa6001282e5b0ff5dd5e3449d8
MateoAlvarez-Dev <matheoz2003@gmail.com>
2024-07-29 12:40:59 -0500

    Merge completed

--- target/generated-sources/annotations/com/riwi/assentment_project/util/mappers/CouponMapperImpl.java
+++ target/generated-sources/annotations/com/riwi/assentment_project/util/mappers/CouponMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-29T12:39:34-0500",
+    date = "2024-07-29T12:40:13-0500",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.39.0.v20240620-1855, environment: Java 17.0.11 (Eclipse Adoptium)"
 )
 @Component
